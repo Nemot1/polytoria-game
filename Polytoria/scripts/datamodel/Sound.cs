3779dee7002bad0bcd8f0da0c3f39238f7986e5b
arlocomotive <113862557+arlocomotive@users.noreply.github.com>
2026-05-24 02:00:10 -0500

    Play sounds on the server (#450)
    * allow sounds to play on the server
    
    Signed-off-by: arlocomotive <arloyamins@gmail.com>
    
    * changed comments
    
    Signed-off-by: arlocomotive <arloyamins@gmail.com>
    
    ---------
    
    Signed-off-by: arlocomotive <arloyamins@gmail.com>

--- Polytoria/scripts/datamodel/Sound.cs
+++ Polytoria/scripts/datamodel/Sound.cs
@@ -403,8 +403,6 @@ public sealed partial class Sound : Dynamic
 			{
 				ServerIsPlaying = true;
 			}
-			// Mute audio on server
-			if (Root.Network.IsServer) return;
 			_audioPlayer?.Play();
 			_audioPlayer3D?.Play();
 		}
@@ -416,6 +414,9 @@ public sealed partial class Sound : Dynamic
 
 	private void InternalPlayOneShot(float volume)
 	{
+		// can safely mute on the server since this method doesn't change any properties
+		if (Root.Network.IsServer) return;
+
 		if (_audioPlayer != null)
 		{
 			AudioStreamPlayer clone = (AudioStreamPlayer)_audioPlayer.Duplicate();
